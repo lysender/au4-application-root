2b4ab6183f91d67fe65e408c45fb2b94352c88ad
Lysender <lysender@darkstar-win>
2023-12-22 12:42:13 +0800

    fix: serve static, catch all using index

--- src/run.rs
+++ src/run.rs
@@ -1,13 +1,11 @@
 
-use std::path::PathBuf;
-use std::process;
+use std::path::{PathBuf, Path};
 use axum::http::Method;
 use axum::Router;
-use axum::routing::get_service;
-use clap::Parser;
+use axum::response::{IntoResponse, Html};
+use axum::routing::{get_service, get};
 use tokio::net::TcpListener;
-use tower_http::cors::{CorsLayer, Any};
-use tower_http::services::ServeDir;
+use tower_http::services::{ServeDir, ServeFile};
 use tower::ServiceBuilder;
 use tracing::Level;
 use tower_http::trace::{TraceLayer, DefaultMakeSpan, DefaultOnResponse};
@@ -18,7 +16,9 @@ use crate::config::Config;
 
 pub async fn run(config: &Config) -> Result<()> {
     let routes_all = Router::new()
+        .merge(routes_index())
         .merge(routes_static(&config.public_dir))
+        .fallback_service(routes_fallback())
         .layer(ServiceBuilder::new()
             .layer(TraceLayer::new_for_http()
                 .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
@@ -40,5 +40,25 @@ pub async fn run(config: &Config) -> Result<()> {
 }
 
 fn routes_static(dir: &PathBuf) -> Router {
-    Router::new().nest_service("/", get_service(ServeDir::new(dir)))
+    Router::new()
+        .nest_service("/assets", get_service(ServeDir::new(dir.join("assets"))))
+        .nest_service("/css", get_service(ServeDir::new(dir.join("css"))))
+        .nest_service("/images", get_service(ServeDir::new(dir.join("images"))))
+        .nest_service("/js", get_service(ServeDir::new(dir.join("js"))))
+}
+
+fn routes_index() -> Router {
+    Router::new()
+        .route("/", get(handler_index))
+}
+
+fn routes_fallback() -> Router {
+    Router::new()
+        .nest_service("/", get(handler_index))
+}
+
+async fn handler_index() -> impl IntoResponse {
+    info!("this is from index...");
+
+    Html("This is the index page...")
 }
