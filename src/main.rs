dcf3fb9bfa500af0b95c0194800d1ced2aee71df
Lysender <lysender@darkstar-win>
2025-02-21 09:18:14 +0800

    fix: update version

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,6 @@ pub use error::{Error, Result};
 
 #[tokio::main]
 async fn main() {
-    // Set the RUST_LOG, if it hasn't been explicitly defined
-    if std::env::var_os("RUST_LOG").is_none() {
-        std::env::set_var("RUST_LOG", "appplication_root=info,tower_http=info")
-    }
-
     tracing_subscriber::fmt()
         .with_target(false)
         .compact()
